5b42980579a8511a8224da6702a51a31cbf89cee
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-03-20 09:46:37 +0700

    VD1_ChuyenDoiManHinh
    Hoàn thành giao diện 2 màn hình

---
+++ VD1_SimpleExplicitIntents/app/src/main/java/ntu/hieu/vd1_simpleexplicitintents/MainActivity2.java
@@ -0,0 +1,14 @@
+package ntu.hieu.vd1_simpleexplicitintents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
