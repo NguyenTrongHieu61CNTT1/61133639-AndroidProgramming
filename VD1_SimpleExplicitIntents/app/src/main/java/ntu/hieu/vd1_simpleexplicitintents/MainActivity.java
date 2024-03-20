c913c12358c801c37ffcad00b473c703384eda79
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-03-20 10:06:04 +0700

    VD1_ChuyenDoiMH
    Hoàn thành xử lý onClick trên các button và đã hoạt động

--- VD1_SimpleExplicitIntents/app/src/main/java/ntu/hieu/vd1_simpleexplicitintents/MainActivity.java
+++ VD1_SimpleExplicitIntents/app/src/main/java/ntu/hieu/vd1_simpleexplicitintents/MainActivity.java
@@ -2,7 +2,9 @@ package ntu.hieu.vd1_simpleexplicitintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //Hàm xử lý khi nhấn nút sẽ chuyển đổi sang màn hình khác
+    public void chuyenMH(View v){
+        //Tạo đối tượng Intent
+        //Tham số thứ 2 của hàm là tên màn hình (Activity) muốn chuyển sang
+        Intent iMHPhu = new Intent(this, MainActivity2.class);
+        //Thực hiện chuyển đổi
+        startActivity(iMHPhu);
     }
 }
--- VD1_SimpleExplicitIntents/app/src/main/java/ntu/hieu/vd1_simpleexplicitintents/MainActivity2.java
+++ VD1_SimpleExplicitIntents/app/src/main/java/ntu/hieu/vd1_simpleexplicitintents/MainActivity2.java
@@ -2,7 +2,9 @@ package ntu.hieu.vd1_simpleexplicitintents;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+    //Thực hiẹn xử lý quay về trang chủ
+    public void quayVe(View v){
+        //Tạo đối tượng Intent
+        //Tham số thứ 2 truyền vào sẽ là màn hình chính
+        Intent iMHChinh = new Intent(this, MainActivity.class);
+        //Thực hiện chuyển
+        startActivity(iMHChinh);
     }
 }
