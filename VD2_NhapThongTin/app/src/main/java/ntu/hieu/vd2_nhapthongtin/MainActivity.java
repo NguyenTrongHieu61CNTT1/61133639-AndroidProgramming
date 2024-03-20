88dad57fb96c2a8fcfbf7920dea0a685867e5acb
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-03-20 12:53:48 +0700

    Cập nhật VD2_NhapLieu
    chưa hoàn thành xử lý back-end, đang đợi hoàn thành

--- VD2_NhapThongTin/app/src/main/java/ntu/hieu/vd2_nhapthongtin/MainActivity.java
+++ VD2_NhapThongTin/app/src/main/java/ntu/hieu/vd2_nhapthongtin/MainActivity.java
@@ -1,8 +1,11 @@
 package ntu.hieu.vd2_nhapthongtin;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void nhapLieu(View v){
+        //Tạo mới 1 Intent
+        Intent iNhap = new Intent(this, MainActivity2.class);
+        startActivity(iNhap,8000); //Code yêu cầu: 8000
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == 8000){
+            if (resultCode == RESULT_OK){
+                String hoTenNhanDuoc = data.getStringExtra();
+            }
+        }
     }
 }
--- VD2_NhapThongTin/app/src/main/java/ntu/hieu/vd2_nhapthongtin/MainActivity2.java
+++ VD2_NhapThongTin/app/src/main/java/ntu/hieu/vd2_nhapthongtin/MainActivity2.java
@@ -3,6 +3,8 @@ package ntu.hieu.vd2_nhapthongtin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity2 extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void nhapQuayVe(View v){
+        EditText edHT = (EditText)findViewById(r.id);
     }
 }
