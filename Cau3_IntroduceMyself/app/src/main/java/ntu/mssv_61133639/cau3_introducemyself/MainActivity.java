057f7ba870740c8b30e9a1bd04a6de70ed55a4a0
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-03-23 16:33:33 +0700

    Cau3_IntroduceMyself
    Thêm background, icon và các biến màu cần thiết cho việc thiết kế app

---
+++ Cau3_IntroduceMyself/app/src/main/java/ntu/mssv_61133639/cau3_introducemyself/MainActivity.java
@@ -0,0 +1,14 @@
+package ntu.mssv_61133639.cau3_introducemyself;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
