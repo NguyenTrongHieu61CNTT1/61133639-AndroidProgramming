51d277cfe465c7e099e1a910fab98a0c525083f9
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-03-23 20:44:59 +0700

    Cau1_AppCalendar
    Sử dụng thẻ calendarView, giao diện còn đơn giản

---
+++ Cau1_AppCalendar/app/src/main/java/ntu/mssv_61133639/cau1_appcalendar/MainActivity.java
@@ -0,0 +1,17 @@
+package ntu.mssv_61133639.cau1_appcalendar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
