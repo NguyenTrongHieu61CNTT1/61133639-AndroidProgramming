4cb977c07e72afb57d91a6fc4fa62d1df85cc59e
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-03-24 09:34:45 +0700

    Cập nhật Cau2_AppCalendar
    Thêm hàm hiển thị thông báo ngày hiện tại khi vừa mở ứng dụng

--- Cau2_AppCalendar/app/src/main/java/ntu/mssv_61133639/cau1_appcalendar/MainActivity.java
+++ Cau2_AppCalendar/app/src/main/java/ntu/mssv_61133639/cau1_appcalendar/MainActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import android.widget.CalendarView;
 import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,15 +26,27 @@ public class MainActivity extends AppCompatActivity {
         calendar = Calendar.getInstance();
 
         //Set ngày mặc định khi khởi chạy ứng dụng
-        setDay(1,1,2024);
+        //setDay(1,1,2024);
+
+        //Hiển thị thông báo ngày hiện tại khi vừa vào ứng dụng
+        getDate();
 
         //Tạo Listener khi thay đổi ngày sẽ hiện thông báo ngày bạn đang chọn
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int day) {
-                Toast.makeText(MainActivity.this, "Bạn đang chọn ngày: " + day + "/" + month + "/" + year, Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Bạn đang chọn ngày: " + day + "/" + month + 1 + "/" + year, Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+    //Hàm hiển thị thông báo ngày hiện tại
+    public void getDate(){
+        long date = calendarView.getDate();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yy", Locale.getDefault());
+        calendar.setTimeInMillis(date);
+        String selected_date = simpleDateFormat.format(calendar.getTime());
+        Toast.makeText(this, selected_date, Toast.LENGTH_SHORT).show();
     }
 
     //Hàm set ngày tháng năm mặc định
