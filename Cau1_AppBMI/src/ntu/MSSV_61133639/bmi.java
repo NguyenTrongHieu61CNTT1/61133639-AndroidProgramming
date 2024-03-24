e652b869de57406e73c51d99c063ba731645c5e1
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-03-24 11:28:16 +0700

    Cập nhật Cau1_AppBMI
    Đặt tên id các thành phần

--- Cau1_AppBMI/src/ntu/MSSV_61133639/bmi.java
+++ Cau1_AppBMI/src/ntu/MSSV_61133639/bmi.java
@@ -11,18 +11,17 @@ import java.awt.Font;
 import javax.swing.JTextField;
 import javax.swing.JRadioButton;
 import java.awt.Color;
+import javax.swing.JButton;
 
 public class bmi extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
+	private JTextField canNangValues;
+	private JTextField chieuCaoValues;
+	private JTextField kQ;
 
-	/**
-	 * Launch the application.
-	 */
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -36,64 +35,67 @@ public class bmi extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	
 	public bmi() {
 		setTitle("Ứng dụng BMI");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 640, 298);
+		setBounds(100, 100, 640, 320);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JLabel lblNewLabel = new JLabel("Cân nặng:");
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel.setBounds(48, 106, 77, 28);
-		contentPane.add(lblNewLabel);
+		JLabel canNang = new JLabel("Cân nặng:");
+		canNang.setFont(new Font("Tahoma", Font.BOLD, 15));
+		canNang.setBounds(48, 106, 77, 28);
+		contentPane.add(canNang);
 		
-		JLabel lblChiuCao = new JLabel("Chiều cao:");
-		lblChiuCao.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblChiuCao.setBounds(48, 158, 94, 28);
-		contentPane.add(lblChiuCao);
+		JLabel chieuCao = new JLabel("Chiều cao:");
+		chieuCao.setFont(new Font("Tahoma", Font.BOLD, 15));
+		chieuCao.setBounds(48, 158, 94, 28);
+		contentPane.add(chieuCao);
 		
 		JLabel lblNewLabel_1 = new JLabel("ỨNG DỤNG THEO DÕI CHỈ SỐ BMI");
 		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		lblNewLabel_1.setBounds(169, 35, 313, 46);
 		contentPane.add(lblNewLabel_1);
 		
-		textField = new JTextField();
-		textField.setBounds(135, 100, 96, 46);
-		contentPane.add(textField);
-		textField.setColumns(10);
+		canNangValues = new JTextField();
+		canNangValues.setBounds(135, 100, 96, 46);
+		contentPane.add(canNangValues);
+		canNangValues.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(135, 152, 96, 46);
-		contentPane.add(textField_1);
+		chieuCaoValues = new JTextField();
+		chieuCaoValues.setColumns(10);
+		chieuCaoValues.setBounds(135, 152, 96, 46);
+		contentPane.add(chieuCaoValues);
 		
-		JRadioButton rdbtnNewRadioButton = new JRadioButton("Kilogams");
-		rdbtnNewRadioButton.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		rdbtnNewRadioButton.setBounds(250, 112, 103, 21);
-		contentPane.add(rdbtnNewRadioButton);
+		JRadioButton rdKg = new JRadioButton("Kilogams");
+		rdKg.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		rdKg.setBounds(250, 112, 103, 21);
+		contentPane.add(rdKg);
 		
-		JRadioButton rdbtnNewRadioButton_1 = new JRadioButton("Centimets");
-		rdbtnNewRadioButton_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		rdbtnNewRadioButton_1.setBounds(250, 164, 103, 21);
-		contentPane.add(rdbtnNewRadioButton_1);
+		JRadioButton rdCm = new JRadioButton("Centimets");
+		rdCm.setFont(new Font("Tahoma", Font.PLAIN, 15));
+		rdCm.setBounds(250, 164, 103, 21);
+		contentPane.add(rdCm);
 		
-		textField_2 = new JTextField();
-		textField_2.setBackground(new Color(255, 255, 255));
-		textField_2.setEditable(false);
-		textField_2.setBounds(486, 100, 96, 98);
-		contentPane.add(textField_2);
-		textField_2.setColumns(10);
+		kQ = new JTextField();
+		kQ.setBackground(new Color(255, 255, 255));
+		kQ.setEditable(false);
+		kQ.setBounds(486, 100, 96, 98);
+		contentPane.add(kQ);
+		kQ.setColumns(10);
 		
 		JLabel lblNewLabel_2 = new JLabel("KẾT QUẢ:");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.BOLD, 15));
 		lblNewLabel_2.setBounds(399, 100, 77, 98);
 		contentPane.add(lblNewLabel_2);
+		
+		JButton btnKT = new JButton("KIỂM TRA");
+		btnKT.setFont(new Font("Tahoma", Font.BOLD, 15));
+		btnKT.setBounds(48, 211, 183, 46);
+		contentPane.add(btnKT);
 	}
 }
