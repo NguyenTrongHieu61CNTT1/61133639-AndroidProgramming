e800d2ed4c1f597557fd50b2bf609c5ac210cb48
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-04-12 15:54:14 +0700

    UsingRecyclerView
    Hoàn thành xong bước 3

--- UsingRecylerView/app/src/main/java/ntu/mssv_61133639/usingrecylerview/LandScapeAdapter.java
+++ UsingRecylerView/app/src/main/java/ntu/mssv_61133639/usingrecylerview/LandScapeAdapter.java
@@ -1,6 +1,7 @@
 package ntu.mssv_61133639.usingrecylerview;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,7 +9,6 @@ import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
-
 import java.util.ArrayList;
 
 public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.ItemLandHolder> {
@@ -24,17 +24,30 @@ public class LandScapeAdapter extends RecyclerView.Adapter<LandScapeAdapter.Item
     @NonNull
     @Override
     public ItemLandHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        LayoutInflater cai_bom = LayoutInflater.from(context);
+        View vItem = cai_bom.inflate(R.layout.item_land, parent, false);
+        ItemLandHolder viewHolderCreated = new ItemLandHolder(vItem);
+        return viewHolderCreated;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ItemLandHolder holder, int position) {
-
+        //Lấy đổi tượng hiển thị:
+        LandScape landScapeHienThi = lstData.get(position);
+        //Trích thông tin
+        String caption = landScapeHienThi.getLandCaption();
+        String tenFileAnh = landScapeHienThi.getLandImageFileName();
+        //Đặt vào các trường thông tin của holder
+        holder.tvCaption.setText(caption);
+        //Đặt ảnh:
+        String packageName = holder.itemView.getContext().getPackageName();
+        int imageID = holder.itemView.getResources().getIdentifier(tenFileAnh, "mipmap", packageName);
+        holder.ivLandscape.setImageResource(imageID);
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return lstData.size();
     }
 
     class ItemLandHolder extends RecyclerView.ViewHolder{
--- UsingRecylerView/app/src/main/java/ntu/mssv_61133639/usingrecylerview/MainActivity.java
+++ UsingRecylerView/app/src/main/java/ntu/mssv_61133639/usingrecylerview/MainActivity.java
@@ -1,14 +1,41 @@
 package ntu.mssv_61133639.usingrecylerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    LandScapeAdapter landScapeAdapter;
+    ArrayList<LandScape> recyclerViewDatas;
+    RecyclerView recyclerViewLandscape;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Chuẩn bị dữ liệu cho landscapeList
+        recyclerViewDatas = getDataForRecyclerView();
+        //Tìm điều khiển Recycler
+        recyclerViewLandscape = findViewById(R.id.recyclerLand);
+        //Tạo Layout manager để đặt bố cục cho Recycler
+        RecyclerView.LayoutManager layoutLinear = new LinearLayoutManager(this);
+        recyclerViewLandscape.setLayoutManager(layoutLinear);
+        //Tạo Adapter gắn với nguồn dữ liệu
+        landScapeAdapter = new LandScapeAdapter(this, recyclerViewDatas);
+        //Gắn Adapter với Recycler
+        recyclerViewLandscape.setAdapter(landScapeAdapter);
+    }
+
+    ArrayList<LandScape> getDataForRecyclerView(){
+        ArrayList<LandScape> dsDuLieu = new ArrayList<LandScape>();
+        LandScape landScape1 = new LandScape("thaptram", "Tháp Trầm Hương - Biểu tượng Thành phố Nha Trang");
+        dsDuLieu.add(landScape1);
+        dsDuLieu.add(new LandScape("thapba", "Tháp Bà Ponagar - Văn hóa du lịch Nha Trang"));
+        dsDuLieu.add(new LandScape("chualongson", "Chùa Long Sơn - Phật nhìn cả thành phố biển"));
+        return dsDuLieu;
     }
 }
