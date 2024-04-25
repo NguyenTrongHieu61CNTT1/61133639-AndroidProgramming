9a457503a515ce26da2a632c5cf5fe45117e063f
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-04-25 09:35:52 +0700

    VD2_LoginWithIntent
    Thiết kế xong màn hình thứ 3: Màn hình chính

---
+++ VD2_LoginWithIntent/app/src/main/java/ntu/mssv_61133639/vd2_loginwithintent/HomeActivity.java
@@ -0,0 +1,14 @@
+package ntu.mssv_61133639.vd2_loginwithintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
