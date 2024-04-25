7f90d30bb2820d8edde5e166a99bd46ccd07f2cf
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-04-25 10:16:58 +0700

    VD2_LoginWithIntent
    Hoàn thành bài tập

--- VD2_LoginWithIntent/app/src/main/java/ntu/mssv_61133639/vd2_loginwithintent/HomeActivity.java
+++ VD2_LoginWithIntent/app/src/main/java/ntu/mssv_61133639/vd2_loginwithintent/HomeActivity.java
@@ -2,7 +2,9 @@ package ntu.mssv_61133639.vd2_loginwithintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        //Lấy thông tin user:
+        Intent iThongTin = getIntent();
+        //Lọc ra lấy dữ liệu:
+        String tenHienThi = iThongTin.getStringExtra("ten_dang_nhap");
+        //Gắn tên vào điều khiển:
+        TextView tvTenDN = (TextView) findViewById(R.id.ten);
+        tvTenDN.setText(tenHienThi);
     }
 }
