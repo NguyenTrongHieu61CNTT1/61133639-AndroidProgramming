b39ee65906b1ddaf1e166d68798246bcc69c662f
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-04-25 10:06:39 +0700

    VD2_LoginWithIntent
    Xử lý xong đăng nhập chuyển sang màn hình Home với tên đăng nhập: hieu, mật khẩu: 123

--- VD2_LoginWithIntent/app/src/main/java/ntu/mssv_61133639/vd2_loginwithintent/LoginActivity.java
+++ VD2_LoginWithIntent/app/src/main/java/ntu/mssv_61133639/vd2_loginwithintent/LoginActivity.java
@@ -2,7 +2,12 @@ package ntu.mssv_61133639.vd2_loginwithintent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +15,34 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btnXacNhan = (Button) findViewById(R.id.btn_ok);
+
+        btnXacNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Xử lý đăng nhập
+                //B1: Tìm tham chiếu đến điều khiển
+                EditText edtName = (EditText) findViewById(R.id.edt_username);
+                EditText edtPass = (EditText) findViewById(R.id.edt_password);
+                //B2: Lấy dữ liệu
+                String tenDangNhap = edtName.getText().toString();
+                String matKhau = edtPass.getText().toString();
+                //Kiểm tra mật khẩu và tên đăng nhập
+                if (tenDangNhap.equals("hieu") && matKhau.equals("123")){
+                    //Đúng -> home
+                    Intent iHome = new Intent(LoginActivity.this, HomeActivity.class);
+                    //Gói dữ liệu vào HOME (key [Lọc dữ liệu qua key] - value)
+                    iHome.putExtra("ten_dang_nhap", tenDangNhap);
+                    iHome.putExtra("mat_khau", matKhau);
+                    //Gửi đi:
+                    startActivity(iHome);
+                }
+                else {
+                    //Sai -> Thông báo
+                    Toast.makeText(LoginActivity.this, "Bạn đã nhập sai, vui lòng nhập lại!", Toast.LENGTH_LONG);
+                }
+            }
+        });
     }
 }
