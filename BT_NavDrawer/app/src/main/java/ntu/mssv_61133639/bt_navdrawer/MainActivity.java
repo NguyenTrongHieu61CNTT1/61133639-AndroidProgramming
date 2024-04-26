a8acb3928b600505ea51eef9ece136ccef406dd2
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-04-26 14:59:02 +0700

    BT_NavDrawer
    Tạo dự án

---
+++ BT_NavDrawer/app/src/main/java/ntu/mssv_61133639/bt_navdrawer/MainActivity.java
@@ -0,0 +1,14 @@
+package ntu.mssv_61133639.bt_navdrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
