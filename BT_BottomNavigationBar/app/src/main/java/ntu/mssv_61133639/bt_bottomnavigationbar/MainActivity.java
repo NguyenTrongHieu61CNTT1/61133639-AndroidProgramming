64d55ba28c33bcce5d12c2c822116a974516f223
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-04-26 14:12:38 +0700

    BT_BottomNavBar
    tạo các fragment các màn hình để thực hiện chuyển đổi màn hình

--- BT_BottomNavigationBar/app/src/main/java/ntu/mssv_61133639/bt_bottomnavigationbar/MainActivity.java
+++ BT_BottomNavigationBar/app/src/main/java/ntu/mssv_61133639/bt_bottomnavigationbar/MainActivity.java
@@ -8,10 +8,17 @@ import android.widget.FrameLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.b_NavBar);
+        frameLayout = findViewById(R.id.frameLayout);
+
     }
 }
