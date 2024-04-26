3906e5f698cdb413f6dd60a43e89f055ffae4131
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-04-26 16:16:04 +0700

    BT_NavDrawer
    Hoàn thành

--- BT_NavDrawer/app/src/main/java/ntu/mssv_61133639/bt_navdrawer/MainActivity.java
+++ BT_NavDrawer/app/src/main/java/ntu/mssv_61133639/bt_navdrawer/MainActivity.java
@@ -1,14 +1,53 @@
 package ntu.mssv_61133639.bt_navdrawer;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ActionBarDrawerToggle drawerToggle;
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item)){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        drawerToggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.open, R.string.close);
+        drawerLayout.addDrawerListener(drawerToggle);
+        drawerToggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if (item.getItemId() == R.id.home){
+                    Toast.makeText(MainActivity.this, "Bạn đã chọn trang chủ", Toast.LENGTH_LONG).show();
+                } else if (item.getItemId() == R.id.message) {
+                    Toast.makeText(MainActivity.this, "Bạn đã chọn tin nhắn", Toast.LENGTH_LONG).show();
+                } else if (item.getItemId() == R.id.photo) {
+                    Toast.makeText(MainActivity.this, "Bạn đã chọn hình ảnh", Toast.LENGTH_LONG).show();
+                }
+                return false;
+            }
+        });
     }
 }
