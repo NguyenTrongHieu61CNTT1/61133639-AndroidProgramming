a567b526bf3dfeed41cb3a248a7867ed79678910
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-04-26 14:01:25 +0700

    BT_BottomNavigationBar
    Thiết kế xong giao diện navigation bar

--- BT_BottomNavigationBar/app/src/main/java/ntu/mssv_61133639/bt_bottomnavigationbar/MainActivity.java
+++ BT_BottomNavigationBar/app/src/main/java/ntu/mssv_61133639/bt_bottomnavigationbar/MainActivity.java
@@ -3,9 +3,12 @@ package ntu.mssv_61133639.bt_bottomnavigationbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
