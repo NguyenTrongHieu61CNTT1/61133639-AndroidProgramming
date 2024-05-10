1104a77f6fb9e2911c42d24323a2b0561dbc5f01
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-05-10 11:04:42 +0700

    QLBook_SQLite
    Tạo CSDL - Tạo bảng

--- QLBook_SQLite/app/src/main/java/ntu/mssv_61133639/qlbook_sqlite/MainActivity.java
+++ QLBook_SQLite/app/src/main/java/ntu/mssv_61133639/qlbook_sqlite/MainActivity.java
@@ -2,6 +2,7 @@ package ntu.mssv_61133639.qlbook_sqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Bước 0: Tạo cơ sở dữ liệu:
+        SQLiteDatabase db = openOrCreateDatabase("QLBook.db", MODE_PRIVATE, null); //Tên file db/ giới hạn truy cập/con trỏ bản ghi
+        //Bước 1: Tạo bảng ghi
+        String sqlXoaBangNeuDaCo = "DROP TABLE IF EXISTS Books;"; //Tạo bảng nha!
+        String sqlTaoBang = "CREATE TABLE Books(BookID integer PRIMARY KEY," + "BookName text," + "Page interger," + "Price Float," + "Description text);";
+        //Thực hiện các lệnh SQL:
+        db.execSQL(sqlXoaBangNeuDaCo);
+        db.execSQL(sqlTaoBang);
+        //Close to check:
+        db.close();
     }
 }
