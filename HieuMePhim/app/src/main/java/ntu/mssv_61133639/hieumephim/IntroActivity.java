36de3f17fbf3ff5576ecbb4dad71765f128d163e
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-05-21 14:03:41 +0700

    HieuMePhim
    Xử lý sự kiện nút "Xem ngay" ở trang Giới thiệu

--- HieuMePhim/app/src/main/java/ntu/mssv_61133639/hieumephim/IntroActivity.java
+++ HieuMePhim/app/src/main/java/ntu/mssv_61133639/hieumephim/IntroActivity.java
@@ -2,13 +2,23 @@ package ntu.mssv_61133639.hieumephim;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class IntroActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        Button btn_playnow  = findViewById(R.id.btn_playnow);
+        btn_playnow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(IntroActivity.this, LoginActivity.class));
+            }
+        });
     }
 }
