6c69caa0e357c37e2a2c8f194bc213ddef375a2a
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-05-22 16:11:27 +0700

    HieuMePhim
    Em Implement thư viện glide để xử lý hình ảnh nhưng do còn một vài lỗi cần được fix ạ

--- HieuMePhim/app/build.gradle.kts
+++ HieuMePhim/app/build.gradle.kts
@@ -33,7 +33,8 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.viewpager2:viewpager2:1.1.0")
+    implementation("androidx.viewpager2:viewpager2:1.1.0") //Thư viện ViewPager2
+    implementation ("com.github.bumptech.glide:glide:4.14.2") //Thư viện bumptech/glide: tải hình ảnh và hiển thị lên ImageView
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- HieuMePhim/app/src/main/java/ntu/mssv_61133639/hieumephim/Adapters/SliderAdapters.java
+++ HieuMePhim/app/src/main/java/ntu/mssv_61133639/hieumephim/Adapters/SliderAdapters.java
@@ -1,4 +1,67 @@
 package ntu.mssv_61133639.hieumephim.Adapters;
 
-public class SliderAdapters {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.List;
+
+import ntu.mssv_61133639.hieumephim.Domain.SliderItems;
+import ntu.mssv_61133639.hieumephim.R;
+
+public class SliderAdapters extends RecyclerView.Adapter<SliderAdapters.SliderViewHolder> {
+
+    //Khai báo:
+    private List<SliderItems> sliderItems;
+    private ViewPager2 viewPager2;
+    private Context context;
+
+    //Generate Contructor: sliderItems và viewPager2
+    public SliderAdapters(List<SliderItems> sliderItems, ViewPager2 viewPager2) {
+        this.sliderItems = sliderItems;
+        this.viewPager2 = viewPager2;
+    }
+
+    //Implement Methods
+    @NonNull
+    @Override
+    public SliderAdapters.SliderViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        context = parent.getContext();
+        return new SliderViewHolder(LayoutInflater.from(parent.getContext()).inflate(
+                R.layout.slide_item_container, parent, false
+        ));
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull SliderAdapters.SliderViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class SliderViewHolder extends RecyclerView.ViewHolder{
+        private ImageView imageView;
+
+        //Contructor:
+        public SliderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.imageSlide)
+        }
+        void setImage(SliderItems sliderItems){ //Sử dụng thư viện bumptech/glide
+            //Code sử dụng thư viện glide
+        }
+    }
 }
