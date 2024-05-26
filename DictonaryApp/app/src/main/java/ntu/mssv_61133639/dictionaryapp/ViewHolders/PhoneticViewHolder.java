6d84f6a417a21c51443cfd80519fb15ade1c89d9
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-05-26 15:12:44 +0700

    DictionaryApp
    Thiết kế các thành phần giao diện

---
+++ DictonaryApp/app/src/main/java/ntu/mssv_61133639/dictionaryapp/ViewHolders/PhoneticViewHolder.java
@@ -0,0 +1,12 @@
+package ntu.mssv_61133639.dictionaryapp.ViewHolders;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PhoneticViewHolder extends RecyclerView.ViewHolder {
+    public PhoneticViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+}
