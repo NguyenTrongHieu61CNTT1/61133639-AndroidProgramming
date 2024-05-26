a2c966e097acba05188fba8c4352cb114ce8fab5
NguyenTrongHieu61CNTT1 <91318779+NguyenTrongHieu61CNTT1@users.noreply.github.com>
2024-05-26 11:14:53 +0700

    DictionaryApp
    Tạo dự án và tạo các thuộc tính của API

---
+++ DictonaryApp/app/src/main/java/ntu/mssv_61133639/dictionaryapp/Models/Meanings.java
@@ -0,0 +1,24 @@
+package ntu.mssv_61133639.dictionaryapp.Models;
+
+import java.util.List;
+
+public class Meanings {
+    String partOfSpeech = "";
+    List<Definitions> definitions = null;
+
+    public String getPartOfSpeech() {
+        return partOfSpeech;
+    }
+
+    public void setPartOfSpeech(String partOfSpeech) {
+        this.partOfSpeech = partOfSpeech;
+    }
+
+    public List<Definitions> getDefinitions() {
+        return definitions;
+    }
+
+    public void setDefinitions(List<Definitions> definitions) {
+        this.definitions = definitions;
+    }
+}
---
+++ DictonaryApp/app/src/main/java/ntu/mssv_61133639/dictionaryapp/Models/Phonetics.java
@@ -0,0 +1,22 @@
+package ntu.mssv_61133639.dictionaryapp.Models;
+
+public class Phonetics {
+    String text = "";
+    String audio = "";
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getAudio() {
+        return audio;
+    }
+
+    public void setAudio(String audio) {
+        this.audio = audio;
+    }
+}
